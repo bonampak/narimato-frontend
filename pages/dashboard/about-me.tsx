fd4655f28f8fedda8b9d986cdb7377056410aed7
Eniola Osabiya <eosabiya@gmail.com>
2022-04-04 13:58:11 +0100

    chore: bug fix with about-me "view card" link

--- pages/dashboard/about-me.tsx
+++ pages/dashboard/about-me.tsx
@@ -87,7 +87,7 @@ const AboutMe = () => {
                                                                                 {myGame.rightSwipedCards.map((card: any) => card._id).includes(card._id) ? "✅" : "❌"}
                                                                             </td>
                                                                             <td className="border px-4 py-2 text-yellow-600 border-blue-500 font-medium">
-                                                                                <Link href={`/card/${card._id}`}>
+                                                                                <Link href={`/cards/${card._id}`}>
                                                                                     <a>View Card</a>
                                                                                 </Link>
                                                                             </td>
